4b4be87f246e9e7d539c598d29c59a92b87ff3da
lucas medeiros <contato.julliaizidori@gmail.com>
2021-04-20 14:27:10 -0300

    resolu~]ao dos exercícios de operadores

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,107 @@
+//-----------------------PARTE1---------------------------//
+//inicio do exercício 1//
+//inicio do exercício 2//
+//-----------------------PARTE2---------------------------//
+
+//----------------------------------------------exercício 1---------------------------------------------------//
+
+
+/*const suaIdade = Number(prompt ("Qual a sua idade?"))
+const idadeDxAmigx = Number(prompt ("Qual a idade dx sex melhor amigx?"))
+console.log ("sua idade é maior do que a dx sex amigx?" + (suaIdade > idadeDxAmigx))
+console.log ("diferença :" +" " + (suaIdade - idadeDxAmigx))*/
+
+//---------------------------------------------exercício 2-----------------------------------------------------//
+
+/*const numeroPar = Number(prompt ("Digite um numero par"))
+console.log (numeroPar % 2)
+*/
+//COMENTÁRIO: sempre que for numero par, o reto de 2 vai ser 0 . Quando for impar o resto será 1//
+
+//------------------------------------------exercício 3-------------------------------------------------------//
+
+/*let listaDeTarefas = [" ", " ", " "]
+
+listaDeTarefas[1] = prompt ("Digite uma tarefa que você gosta de fazer")
+listaDeTarefas[2] = prompt ("Digite outra tarefa que você gosta de fazer")
+listaDeTarefas[3] = prompt ("Digite outra tarefa que você gosta de fazer")
+console.log (listaDeTarefas)
+
+let remove = Number(prompt ("qual das tres tarefas você ja realizou: 1, 2 ou 3?"))
+
+listaDeTarefas.splice(remove, 1)
+console.log (listaDeTarefas)*/
+
+
+//----------------------------------------exercício 4------------------------------------------------------//
+
+/*const seuNome = prompt("Qual o seu nome?")
+const seuEmail = prompt("Qual o seu e-mail?")
+
+console.log ("O e-mail " + seuEmail + " foi cadastrado com sucesso!. Seja Bem-Vindx, " + seuNome)*/
+
+//-----------------------DESAFIO---------------------------//
+
+//----------------------------------------------exercício 1---------------------------------------------------//
+
+/*fParaK = ((f - 32) * 5/9 + 273.15)
+cParaF = ((c * 9/5) + 32)*/
+
+/*A-->
+
+let f= 77
+
+const fParaK = ((f - 32) * 5/9 + 273.15)
+console.log(fParaK + "°K")*/
+
+/*B-->
+let c = 80
+
+const cParaF = ((c * 9/5) + 32)
+console.log(cParaF + "°F")*/
+
+/*C-->
+
+let c = Number(prompt("Quantos °c vocÊ deseja converter?"))
+
+let f = ((c * 9/5) + 32)
+console.log( c + "°c é igual a : " +f + "°F")
+
+let k = ((f - 32) * 5/9 + 273.15)
+console.log( c + "°c é igual a : " + k + "°K")*/
+
+//---------------------------------------------exercício 2-----------------------------------------------------//
+
+
+/*let consumo = 280
+
+let valor = (consumo*0.05)
+
+let desconto = (valor*0.15)
+
+console.log ("o valor do consumo é : " + valor + ",00 R$")
+
+console.log ( "O valor com desconto é : " + (valor - desconto) + "R$")*/
+
+//---------------------------------------------exercício 3-----------------------------------------------------//
+/*A-->
+let lb = 20
+let kg = (lb * 0.4536)
+
+console.log ("20lb equivalem a " + kg + "kg")*/
+
+/*G-->
+let lb = Number(prompt ("Quantas Libras você quer converter para kg?"))
+let kg = (lb * 0.4536)
+console.log (lb + " lb, equilavem á " + kg + "kg")*/
+
+
+
+
+
+
+
+
+
+
+
