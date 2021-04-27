5f9320fbaa20744b52336862e6c86a1460fbcb80
lucas medeiros <contato.julliaizidori@gmail.com>
2021-04-27 16:36:38 -0300

    exercicos DOM

---
+++ semana4/planejador/teste.js
@@ -0,0 +1,14 @@
+function clicar() {
+    var a = document.getElementById("area");
+    a.innerHTML = "Clicou!";
+    a.style.background = "red";
+}
+
+function entrar() {
+    var a = document.getElementById("area");
+    a.innerHTML = "Entrou!";
+}
+function sair() {
+    var a = document.getElementById("area");
+    a.innerHTML = "Saiu!";
+}
